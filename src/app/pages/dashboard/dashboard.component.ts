698ef0983f6884a157905aaa3d6ecf0a2ef3fe97
Gabriel Bourgault <dizco@users.noreply.github.com>
2019-10-06 18:42:09 -0400

    Hardcoded generator

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,8 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'ngx-dashboard',
   templateUrl: './dashboard.component.html',
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  participants = [
+    {
+      name: 'Gabriel',
+      picked: '',
+    },
+    {
+      name: 'Myriam',
+      picked: '',
+    },
+    {
+      name: 'Élodie',
+      picked: '',
+    },
+    {
+      name: 'Antoine',
+      picked: '',
+    },
+    {
+      name: 'Jérémie',
+      picked: '',
+    },
+    {
+      name: 'Rosalie',
+      picked: '',
+    },
+    {
+      name: 'Camille',
+      picked: '',
+    },
+    {
+      name: 'Maxime',
+      picked: '',
+    },
+  ];
+
+  selections = [];
+
+  ngOnInit(): void {
+    this.participants.forEach((participant) => {
+      const availableParticipants = this.participants.filter((p) => !p.picked && p !== participant);
+      const randomAvailableParticipant = availableParticipants[Math.floor(Math.random() * availableParticipants.length)];
+      randomAvailableParticipant.picked = participant.name;
+    });
+  }
 }
