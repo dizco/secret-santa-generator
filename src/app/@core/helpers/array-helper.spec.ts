33c67f75d71637221edf45581e25e6aaa2632a4c
Gabriel <dizco@users.noreply.github.com>
2019-10-08 22:46:04 -0400

    Lint

--- src/app/@core/helpers/array-helper.spec.ts
+++ src/app/@core/helpers/array-helper.spec.ts
@@ -3,50 +3,50 @@ import { ArrayHelper } from './array-helper';
 describe('ArrayHelper', () => {
   describe('unique', () => {
     it('should return an empty list', () => {
-      //Arrange & Act
+      // Arrange & Act
       const result = ArrayHelper.unique([]);
 
-      //Assert
+      // Assert
       expect(result).toEqual([]);
     });
 
     it('should return a single item', () => {
-      //Arrange & Act
+      // Arrange & Act
       const result = ArrayHelper.unique(['a', 'a']);
 
-      //Assert
+      // Assert
       expect(result).toEqual(['a']);
     });
 
     it('should return two items of different types', () => {
-      //Arrange & Act
+      // Arrange & Act
       const result = ArrayHelper.unique([1, 'b', 1, 1, 'b']);
 
-      //Assert
+      // Assert
       expect(result).toEqual([1, 'b']);
     });
   });
 
   describe('completeShuffle', () => {
     it('should return a reversed array', () => {
-      //Arrange
+      // Arrange
       const array = ['a', 'b'];
 
-      //Act
+      // Act
       const result = ArrayHelper.completeShuffle(array);
 
-      //Assert
+      // Assert
       expect(result).toEqual(['b', 'a']);
     });
 
     it('should return a completely shuffled array', () => {
-      //Arrange
+      // Arrange
       const array = [1, 2, 3, 4, 5];
 
-      //Act
+      // Act
       const result = ArrayHelper.completeShuffle(array);
 
-      //Assert
+      // Assert
       array.forEach((element, index) => {
         expect(result[index]).not.toEqual(element, 'Every element of the array should have swaped positions');
       });
@@ -58,24 +58,24 @@ describe('ArrayHelper', () => {
     });
 
     it('should throw because the array only contains 1 element', () => {
-      //Arrange
+      // Arrange
       const array = [1];
 
-      //Act
+      // Act
       const action = () => ArrayHelper.completeShuffle(array);
 
-      //Assert
+      // Assert
       expect(action).toThrow();
     });
 
     it('should throw because the array contains duplicates', () => {
-      //Arrange
+      // Arrange
       const array = [1, 1];
 
-      //Act
+      // Act
       const action = () => ArrayHelper.completeShuffle(array);
 
-      //Assert
+      // Assert
       expect(action).toThrow();
     });
   });
