37a9f196613dc854ca86840d94858dd3d46bd7ad
Gabriel <dizco@users.noreply.github.com>
2019-10-12 12:45:51 -0400

    Publish test results and coverage

--- karma.conf.js
+++ karma.conf.js
@@ -10,19 +10,24 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
+      require('karma-junit-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client:{
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, 'coverage'), reports: [ 'html', 'lcovonly' ],
+      dir: require('path').join(__dirname, 'coverage'),
+      reports: [ 'html', 'lcovonly', 'text-summary', 'cobertura' ],
       fixWebpackSourcePaths: true
     },
     angularCli: {
       environment: 'dev'
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['progress', 'kjhtml', 'junit'],
+    junitReporter: {
+      outputDir: 'test-results'
+    },
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
